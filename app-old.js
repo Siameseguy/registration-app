509e74ffb6f5ee50145ebc007c728247cdd3bdba
Phanousit Syhakhom <psyhakhom@gmail.com>
2017-06-28 21:23:42 -0400

    added edit button and its functions

--- app-old.js
+++ app-old.js
@@ -2,15 +2,48 @@
 const form = document.querySelector('#registrar');
 // get input selector
 const input = document.querySelector('input');
+
+const mainDiv = document.querySelector('.main');
 // get ul selector
 const ul = document.querySelector('#invitedList');
 
+const div = document.createElement('div');
+const filterLabel = document.createElement('label');
+const filterCheckbox = document.createElement('input');
+
+filterLabel.innerHTML = "Hide those who haven't responded";
+filterCheckbox.type = 'checkbox';
+div.appendChild(filterLabel);
+div.appendChild(filterCheckbox);
+mainDiv.insertBefore(div, ul);
+
+filterCheckbox.addEventListener('change', function(e){
+  const isChecked = e.target.checked;
+  const lis = ul.children;
+
+  if(isChecked) {
+    for(let i = 0; i < lis.length; i++) {
+      let li = lis[i];
+      if(li.className === 'responded') {
+        li.style.display = '';
+      } else {
+        li.style.display = 'none';
+      }
+    }
+  } else {
+    for(let i = 0; i < lis.length; i++) {
+      let li = lis[i];
+      li.style.display = '';
+    }
+  }
+});
+
 // write function that creates li elements with a label, checkbox, and remove button
 function createLI(text) {
-   // create li element
+  // create li element
   const li = document.createElement('li');
+  
   // append text of input into li then clear input
-
   const span = document.createElement('span');
   span.innerHTML = text;
   li.appendChild(span);
@@ -25,15 +58,15 @@ function createLI(text) {
   checkbox.type = 'checkbox';
   label.appendChild(checkbox);
 
-  // create edit button
   const editButton = document.createElement('button');
   editButton.innerHTML = 'edit';
   li.appendChild(editButton);
 
   // create remove button
-  const deleteButton = document.createElement('button');
-  deleteButton.innerHTML = 'remove';
-  li.appendChild(deleteButton);
+  const button = document.createElement('button');
+  button.innerHTML = 'remove';
+  li.appendChild(button);
+  
   return li;
 }
 
@@ -67,24 +100,25 @@ ul.addEventListener('click', function(e){
     const button = e.target;
     const li = button.parentNode;
     const ul = li.parentNode;
-    if(button.innerHTML === 'remove') {
+    if(button.textContent === 'remove') {
       ul.removeChild(li);
       e.preventDefault();
-    } else if(button.innerHTML === 'edit') {
+    } else if(button.textContent === 'edit') {
       const span = li.firstElementChild;
       const input = document.createElement('input');
-      input.type = 'text';
       input.value = span.innerHTML;
+      input.type = 'text';
       li.insertBefore(input, span);
       li.removeChild(span);
       button.innerHTML = 'save';
-    } else if(button.innerHTML === 'save') {
+    } else if(button.textContent === 'save') {
       const input = li.firstElementChild;
       const span = document.createElement('span');
       span.innerHTML = input.value;
       li.insertBefore(span, input);
       li.removeChild(input);
+      button.innerHTML = 'edit';
     }
-    
   }
+
 });
